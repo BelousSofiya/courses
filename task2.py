3439f1c030d1fc8de9f393ef15d685c5a732784a
BelousSofiya <122864020+BelousSofiya@users.noreply.github.com>
2023-01-17 10:04:08 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+a = int(input("Write your number, variety a ="))
+b = int(input("Write your number, variety b ="))
+print(f"""a + b = {a + b}
+a - b = {a - b}
+a * b = {a * b}
+a / b = {a / b}
+a**b = {a ** b}
+a//b = {a // b}
+a%b = {a % b}""")
