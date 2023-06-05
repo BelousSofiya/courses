56d22285b44753c09785cbedadc9719cee408c6a
Sofia Bilous <EgikSonya@gmail.com>
2023-06-05 13:08:03 +0300

    module 6

---
+++ JavaScript_homeworks/6_Troubleshoot/m06task02.js
@@ -0,0 +1,19 @@
+﻿// Task02
+// Your task is to create function that returns square number of an argument
+// Function should check if argument is incorrect and return two different
+// exceptions as instances of Error object:
+// 'missing parameter' if argument is not initializated
+// 'parameter is not a number' if argument is Nan
+
+function m06task02(arg) {
+    try{if(arg===undefined) throw (new Error("missing parameter"))
+        //else{if(typeof arg !== 'number') throw (new Error("parameter is not a number"))}}
+        else{if(isNaN(arg)) throw (new Error("parameter is not a number"))}}
+    catch(err){console.log(err.message)}
+};
+
+//Tests
+
+m06task02('a');
+m06task02(1);
+m06task02();
